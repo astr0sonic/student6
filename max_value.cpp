62f19565f666d07932c466363460a807b1b684f0
bu1att <gbulatgbulat@gmail.com>
2024-05-21 00:07:13 +0300

    task1.11

--- max_value.cpp
+++ max_value.cpp
@@ -10,7 +10,7 @@ double calculate(double a, double b, char op) {
     case '*': return a * b;
     case '/': return b != 0 ? a / b : std::numeric_limits<double>::lowest();
     }
-    return 0; // не должно достигаться
+    return 0;
 }
 
 double maxExpressionValue(std::vector<double>& nums, const std::vector<char>& ops, int index = 1) {
@@ -20,21 +20,17 @@ double maxExpressionValue(std::vector<double>& nums, const std::vector<char>& op
 
     double maxValue = std::numeric_limits<double>::lowest();
     for (int i = index; i < nums.size(); ++i) {
-        // Сохраняем текущие значения
+
         double a = nums[index - 1];
         double b = nums[index];
 
-        // Пробуем все операции
         for (char op : ops) {
-            // Вычисляем текущее значение
             double result = calculate(a, b, op);
 
-            // Создаем новый массив чисел
             std::vector<double> newNums = nums;
             newNums[index - 1] = result;
             newNums.erase(newNums.begin() + index);
 
-            // Рекурсивный вызов
             maxValue = std::max(maxValue, maxExpressionValue(newNums, ops, index));
         }
     }
