1142f2da831185ff8fac7bd5e1a030eb5fbe5570
bu1att <gbulatgbulat@gmail.com>
2024-05-21 01:15:04 +0300

    task4

--- volume.cpp
+++ volume.cpp
@@ -3,13 +3,20 @@
 
 int getMaxVolume(int a, int b, int c, int n, int* arr) {
     int maxVolume = 0;
+    int left = 0;
+    int right = n - 1;
 
-    for (int i = 0; i < n; ++i) {
-        for (int j = i + 1; j < n; ++j) {
-            int height = std::min(arr[i], arr[j]);
-            int length = j - i;
-            int volume = b * length * height;
-            maxVolume = std::max(maxVolume, volume);
+    while (left < right) {
+        int height = std::min(arr[left], arr[right]);
+        int length = right - left;
+        int volume = b * length * height;
+        maxVolume = std::max(maxVolume, volume);
+
+        if (arr[left] < arr[right]) {
+            left++;
+        }
+        else {
+            right--;
         }
     }
 
