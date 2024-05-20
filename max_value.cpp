d7479d00bf9c1fb53103263337d9f1a0a9fbf94e
bu1att <gbulatgbulat@gmail.com>
2024-05-21 00:41:46 +0300

    task1

--- max_value.cpp
+++ max_value.cpp
@@ -1,6 +1,7 @@
 #include "max_value.h"
 #include <algorithm>
 #include <limits>
+#include <vector>
 
 using namespace std;
 
@@ -14,23 +15,37 @@ double calculate(double a, double b, char op) {
     return numeric_limits<double>::lowest();
 }
 
-double calculateMaxValue(double* nums, int n) {
-    if (n < 2) return numeric_limits<double>::lowest();
+double evaluateExpression(vector<double> nums, vector<char> ops) {
+    double result = nums[0];
+    for (int i = 0; i < ops.size(); ++i) {
+        result = calculate(result, nums[i + 1], ops[i]);
+    }
+    return result;
+}
 
-    vector<char> ops = { '+', '-', '*', '/' };
+void findMaxValue(vector<double> nums, vector<char>& ops, int index, double& max_value) {
+    if (index == nums.size() - 1) {
+        double value = evaluateExpression(nums, ops);
+        max_value = max(max_value, value);
+        return;
+    }
+
+    vector<char> operators = { '+', '-', '*', '/' };
+    for (char op : operators) {
+        ops[index] = op;
+        findMaxValue(nums, ops, index + 1, max_value);
+    }
+}
+
+double calculateMaxValue(double* nums, int n) {
+    vector<double> numbers(nums, nums + n);
     double max_value = numeric_limits<double>::lowest();
 
-    sort(nums, nums + n);
+    sort(numbers.begin(), numbers.end());
     do {
-        for (char op1 : ops) {
-            for (char op2 : ops) {
-                for (char op3 : ops) {
-                    double result = calculate(calculate(calculate(nums[0], nums[1], op1), nums[2], op2), nums[3], op3);
-                    max_value = max(max_value, result);
-                }
-            }
-        }
-    } while (next_permutation(nums, nums + n));
+        vector<char> ops(n - 1);
+        findMaxValue(numbers, ops, 0, max_value);
+    } while (next_permutation(numbers.begin(), numbers.end()));
 
     return max_value;
 }
