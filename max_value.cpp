a5ea5de0242b27eee4358f4db72a1a11f5cbec36
bu1att <gbulatgbulat@gmail.com>
2024-05-21 01:20:01 +0300

    task1

--- max_value.cpp
+++ max_value.cpp
@@ -1,58 +1,43 @@
-#include "max_value.h"
-#include <algorithm>
-#include <limits>
-#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
 
-using namespace std;
-
-double calculate(double a, double b, char op) {
+double performOperation(double a, double b, char op) {
     switch (op) {
     case '+': return a + b;
     case '-': return a - b;
     case '*': return a * b;
-    case '/': return b != 0 ? a / b : numeric_limits<double>::lowest();
+    case '/': return b != 0 ? a / b : 1e-9; 
+    default: return 0;
     }
-    return numeric_limits<double>::lowest();
 }
 
-double evaluateExpression(vector<double> nums, vector<char> ops) {
-    double result = nums[0];
-    for (int i = 0; i < ops.size(); ++i) {
-        result = calculate(result, nums[i + 1], ops[i]);
+double recursiveMaxValue(double* nums, int n, int current) {
+    if (current == n - 1) {
+        return nums[current];
     }
-    return result;
+
+    double maxValue = -1e9;
+
+    for (int i = current + 1; i < n; i++) {
+        double left = nums[current];
+        double right = recursiveMaxValue(nums, n, i);
+
+        double addResult = performOperation(left, right, '+');
+        double subResult = performOperation(left, right, '-');
+        double mulResult = performOperation(left, right, '*');
+        double divResult = performOperation(left, right, '/');
+
+        if (addResult > maxValue) maxValue = addResult;
+        if (subResult > maxValue) maxValue = subResult;
+        if (mulResult > maxValue) maxValue = mulResult;
+        if (divResult > maxValue) maxValue = divResult;
+    }
+
+    return maxValue;
 }
 
 double calculateMaxValue(double* nums, int n) {
-    if (n < 2) return numeric_limits<double>::lowest();
-
-    vector<char> ops = { '+', '-', '*', '/' };
-    double max_value = numeric_limits<double>::lowest();
-    vector<char> current_ops(n - 1);
-
-    sort(nums, nums + n);
-    do {
-        vector<int> indices(n - 1, 0);
-
-        while (true) {
-            for (int i = 0; i < n - 1; ++i) {
-                current_ops[i] = ops[indices[i]];
-            }
-
-            double result = evaluateExpression(vector<double>(nums, nums + n), current_ops);
-            max_value = max(max_value, result);
-
-            int pos = n - 2;
-            while (pos >= 0 && indices[pos] == 3) {
-                indices[pos] = 0;
-                --pos;
-            }
-
-            if (pos < 0) break;
-
-            ++indices[pos];
-        }
-    } while (next_permutation(nums, nums + n));
-
-    return max_value;
+    if (n <= 0) return 0.0;
+    return recursiveMaxValue(nums, n, 0);
 }
+
