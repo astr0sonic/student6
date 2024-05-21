e64de72a55141449a626177f5569877cf01a82d5
bu1att <gbulatgbulat@gmail.com>
2024-05-21 11:29:10 +0300

    task1

--- max_value.cpp
+++ max_value.cpp
@@ -1,25 +1,24 @@
-#include <iostream>
+#include "max_value.h"
 #include <vector>
 #include <algorithm>
 #include <limits>
-using namespace std;
 
 double calculate(double a, double b, char op) {
     switch (op) {
     case '+': return a + b;
     case '-': return a - b;
     case '*': return a * b;
-    case '/': return b != 0 ? a / b : numeric_limits<double>::lowest();
+    case '/': return b != 0 ? a / b : std::numeric_limits<double>::lowest();
     }
     return 0;
 }
 
-double maxExpressionValue(vector<double>& nums, vector<char>& ops, int index = 1) {
+double maxExpressionValue(std::vector<double>& nums, const std::vector<char>& ops, int index = 1) {
     if (index == nums.size()) {
         return nums[0];
     }
 
-    double maxValue = numeric_limits<double>::lowest();
+    double maxValue = std::numeric_limits<double>::lowest();
     for (int i = index; i < nums.size(); ++i) {
         double a = nums[index - 1];
         double b = nums[index];
@@ -27,13 +26,18 @@ double maxExpressionValue(vector<double>& nums, vector<char>& ops, int index = 1
         for (char op : ops) {
             double result = calculate(a, b, op);
 
-            vector<double> newNums = nums;
+            std::vector<double> newNums = nums;
             newNums[index - 1] = result;
             newNums.erase(newNums.begin() + index);
 
-            maxValue = max(maxValue, maxExpressionValue(newNums, ops, index));
+            maxValue = std::max(maxValue, maxExpressionValue(newNums, ops, index));
         }
     }
     return maxValue;
 }
 
+double calculateMaxValue(double* nums, int n) {
+    std::vector<double> numVec(nums, nums + n);
+    std::vector<char> ops = { '+', '-', '*', '/' };
+    return maxExpressionValue(numVec, ops);
+}
