f9762bdc9ad5cb7ce36503a04b513279c53d601e
bu1att <gbulatgbulat@gmail.com>
2024-05-23 20:51:43 +0300

    task4

--- volume.cpp
+++ volume.cpp
@@ -1,19 +1,28 @@
 #include "volume.h"
 #include <algorithm>
 
-int getMaxVolume(int a, int b, int c, int n, int* arr) {
-    int maxVolume = 0;
+using namespace std;
 
-    for (int i = 0; i < n; ++i) {
-        for (int j = i + 1; j < n; ++j) {
-            int height = std::min(arr[i], arr[j]);
-            if (height <= c) {
-                int length = j - i;
-                int volume = length * b * height;
-                maxVolume = std::max(maxVolume, volume);
-            }
-        }
-    }
-
-    return maxVolume;
+int getMaxVolume(int a, int b, int c, int n, int* t) {
+	int i = 0, j = n - 1, m = 0;
+	for (j = 0; j + a < n; j++) {
+		int k = min(t[j], t[j + a]);
+		if (k > m && k <= c) {
+			i = j;
+			m = k;
+		}
+	}
+	j = i + a;
+	if (j >= n)
+		j = n - 1;
+	while (i < j) {
+		int k = (j - i) * min(t[i], t[j]);
+		if (k > m && min(t[i], t[j]) <= c)
+			m = k;
+		if (t[i] == min(t[i], t[j]))
+			i++;
+		else
+			j--;
+	}
+	return b * m;
 }
